7eb2f67442a7cc268fead4f5be2f2e35736e2bf2
Ingan121 <lssaa91192@gmail.com>
2025-04-07 23:17:10 +0900

    Fix non-Spicetify dialog styles for ,57?~,61

--- theme/dist/theme.js
+++ theme/dist/theme.js
@@ -242,7 +242,7 @@
                 </svg>
             </button>
             <p>${d.CONF_ABOUT_DESC}</p>
-            <p>${d.CONF_ABOUT_VERSION}: 1.0 Release Candidate 3<span id="wmpotify-about-ctewh-ver"></span></p>
+            <p>${d.CONF_ABOUT_VERSION}: 1.0 Release Candidate 4<span id="wmpotify-about-ctewh-ver"></span></p>
             <p>${d.CONF_ABOUT_AUTHOR} - <a href="https://www.ingan121.com/" target="_blank">www.ingan121.com</a></p>
             <input type="checkbox" id="wmpotify-config-auto-updates" class="wmpotify-aero" checked>
             <label for="wmpotify-config-auto-updates">${d.CONF_ABOUT_AUTO_UPDATES}</label>
