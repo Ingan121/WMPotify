9e90595ba4acbcb3583c58661eebb1129388a229
Ingan121 <lssaa91192@gmail.com>
2025-06-26 23:55:31 +0900

    * Deal with massive SVG changes in 1.2.66 * Fix LibX header not being clickable when scrolled

--- theme/src/js/pages/config.js
+++ theme/src/js/pages/config.js
@@ -147,7 +147,7 @@ function init() {
                 </svg>
             </button>
             <p>${Strings['CONF_ABOUT_DESC']}</p>
-            <p>${Strings['CONF_ABOUT_VERSION']}: 1.1 (Pre-release 2025-06-25)<span id="wmpotify-about-ctewh-ver"></span></p>
+            <p>${Strings['CONF_ABOUT_VERSION']}: 1.1 (Pre-release 2025-06-26)<span id="wmpotify-about-ctewh-ver"></span></p>
             <p>${Strings['CONF_ABOUT_AUTHOR']} - <a href="https://www.ingan121.com/" target="_blank">www.ingan121.com</a></p>
             <input type="checkbox" id="wmpotify-config-auto-updates" class="wmpotify-aero" checked>
             <label for="wmpotify-config-auto-updates">${Strings['CONF_ABOUT_AUTO_UPDATES']}</label>
